d939d97330256886c986a52f88f0cf7656046ed1
yoderale <yoderale@gmail.com>
2025-12-05 15:53:50 -0600

    wip

--- learning_python_projects/chapter_four/pizzas.py
+++ learning_python_projects/chapter_four/pizzas.py
@@ -1,4 +1,5 @@
 #I LOVE PIZZA PIE
-pizzas = [meat lovers, pepperoni, hawaiian]
+pizzas = ['meat lovers', 'pepperoni', 'hawaiian']
 for pizza in pizzas:
-	print(pizza)
+	print(f'I like {pizza.title()} pizza! Its my favorite!')
+print("It's better than life itself!")
