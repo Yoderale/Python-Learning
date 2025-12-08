f7f3f0f4b8b4d26aee8f2c4a37b318f6e171eadb
yoderale <yoderale@gmail.com>
2025-12-07 19:59:04 -0600

    wip

---
+++ learning_python_projects/chapter_four/counting_to_twenty.py
@@ -0,0 +1,11 @@
+values = []
+for value in range(1,21):
+	print(value)
+
+values = list(range(1,21))
+for value in values:
+	print(value)
+
+values = list()
+for value in range(1,21):
+	print(value)
